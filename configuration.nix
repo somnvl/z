0676f44f4c78d8c39965ebcd2e50bdc8479c7742
winter <sophie.menvielle@gmail.com>
2026-01-09 01:51:10 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,5 @@
   console.keyMap = "fr";
   users.users.so = { isNormalUser = true; extraGroups = [ "wheel" ]; };
   environment.systemPackages = with pkgs; [ vim git wget tree ];
-  programs.firefox.enable = true;
-  fonts.packages = with pkgs; [ nerdfonts-jetbrains-mono ];
   system.stateVersion = "25.11";
 }
