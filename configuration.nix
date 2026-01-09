13df1d785ae3b51d84dd597e4f893de7e519692d
winter <sophie.menvielle@gmail.com>
2026-01-09 01:50:08 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,49 +1,17 @@
-{ config, lib, pkgs, ... }:
-
-{
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
-
+{ config, pkgs, ... }: {
+  imports = [ ./hardware-configuration.nix ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";  # ← AJOUTE ÇA !
-
+  boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.supportedFilesystems = [ "btrfs" ];
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-
   time.timeZone = "Europe/Paris";
-
-  users.users.so = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    packages = with pkgs; [
-      tree
-    ];
-  };
-
+  i18n.defaultLocale = "fr_FR.UTF-8";
+  console.keyMap = "fr";
+  users.users.so = { isNormalUser = true; extraGroups = [ "wheel" ]; };
+  environment.systemPackages = with pkgs; [ vim git wget tree ];
   programs.firefox.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    alacritty
-    git
-  ];
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-  ];
-
-  fileSystems."/boot" = {
-    device = "/dev/nvme0n1p7";
-    fsType = "vfat";
-  };
-
-# Et dans hardware-configuration.nix (généré), ça doit matcher
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  fonts.packages = with pkgs; [ nerdfonts-jetbrains-mono ];
   system.stateVersion = "25.11";
-
 }
